e53eb32845cd525d2509f2f2a5efb1235d945afe
Uodditu <waddingtonf@outlook.com>
2022-06-27 01:52:19 -0300

    port again

--- app.py
+++ app.py
@@ -74,6 +74,5 @@ def logout():
     return redirect(url_for('login'))
 
 if __name__ == '__main__':
-    # port = int(os.getenv('PORT'), '5000')
-    #host='0.0.0.0', port = port
-    app.run()
+    port = int(os.getenv('PORT'), '5000')
+    app.run(host='0.0.0.0', port = port)
